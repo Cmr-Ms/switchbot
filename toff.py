7fc7722ab1375043a59eed1f995e937ccc861ccb
Cmr-Ms <80145877+Cmr-Ms@users.noreply.github.com>
2026-01-29 15:38:05 +0900

    Update toff.py

--- toff.py
+++ toff.py
@@ -9,7 +9,7 @@ bulb_id = "操作したい電球のID"
 time.sleep(
 	winreg.EnumValue(winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE,r"SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes\8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c\238c9fa8-0aad-41ed-83f4-97be242c8f20\29f6c1db-86da-48c5-9fdb-f2b67b1f44da"),0)[1] # PCがスリープするまでの時間をレジストリから取得
 	-60*4 # タスクスケジューラー側で、最後の操作から4分経過後にアイドル状態と判定される
-	-20 # APIの実行に少し時間がかかる場合があったため、APIの実行途中にスリープ状態に入ってしまわないよう、少し長めに時間を開ける
+	-20 # APIの実行に少し時間がかかる場合があったため、APIの実行途中にスリープ状態に入ってしまわないよう、少し長めに時間を設ける
 	)
 
 sb = SwitchBot(token=token, secret=secret)
@@ -25,3 +25,4 @@ subprocess.run("rundll32 powrprof.dll,SetSuspendState 0,0,0".split(" ")) # も�
 # 電球が消えてから、PCがスリープ状態になるより前に操作した場合、再度自動で点灯させる
 #kb.read_key()
 #bulb.command("turn_on")
+
